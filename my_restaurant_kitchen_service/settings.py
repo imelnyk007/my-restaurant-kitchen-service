f068797decede8592756f9d0cab6bebc04a0d10b
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-20 15:06:17 +0300

    Add settings for login & logout

--- my_restaurant_kitchen_service/settings.py
+++ my_restaurant_kitchen_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
@@ -56,7 +56,7 @@ ROOT_URLCONF = "my_restaurant_kitchen_service.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"]
+        "DIRS": [os.path.join(BASE_DIR, "templates")]
         ,
         "APP_DIRS": True,
         "OPTIONS": {
@@ -104,6 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 AUTH_USER_MODEL = "kitchen.Cook"
+
+LOGIN_REDIRECT_URL = "/"
 
 
 # Internationalization
