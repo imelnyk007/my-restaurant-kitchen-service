11718ccf3d187c20fa2ce8086b0699f075fca5dd
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-20 15:17:19 +0300

    Add simple Create, Update, Delete views for all models

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from kitchen.models import Category, Dish, Cook
@@ -22,6 +23,23 @@ class CategoryListView(generic.ListView):
     model = Category
 
 
+class CategoryCreateView(generic.CreateView):
+    model = Category
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:category-list")
+
+
+class CategoryUpdateView(generic.UpdateView):
+    model = Category
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:category-list")
+
+
+class CategoryDeleteView(generic.DeleteView):
+    model = Category
+    success_url = reverse_lazy("kitchen:category-list")
+
+
 class DishListView(generic.ListView):
     model = Dish
     queryset = Dish.objects.select_related("category")
@@ -32,6 +50,23 @@ class DishDetailView(generic.DetailView):
     model = Dish
 
 
+class DishCreateView(generic.CreateView):
+    model = Dish
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
+class DishUpdateView(generic.UpdateView):
+    model = Dish
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
+class DishDeleteView(generic.DeleteView):
+    model = Dish
+    success_url = reverse_lazy("kitchen:dish-list")
+
+
 class CookListView(generic.ListView):
     model = Cook
     queryset = Cook.objects.all().prefetch_related("dishes__category")
@@ -40,3 +75,20 @@ class CookListView(generic.ListView):
 
 class CookDetailView(generic.DetailView):
     model = Cook
+
+
+class CookCreateView(generic.CreateView):
+    model = Cook
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:cook-list")
+
+
+class CookUpdateView(generic.UpdateView):
+    model = Cook
+    fields = "__all__"
+    success_url = reverse_lazy("kitchen:cook-list")
+
+
+class CookDeleteView(generic.DeleteView):
+    model = Cook
+    success_url = reverse_lazy("kitchen:cook-list")
