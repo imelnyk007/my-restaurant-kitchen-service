a99c12497fea08d14cf08574a7a875b35508f3bc
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-20 14:40:17 +0300

    Add view index

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
+from kitchen.models import Category, Dish, Cook
+
 
 def index(request):
-    context = {}
+
+    num_category = Category.objects.count()
+    num_dish = Dish.objects.count()
+    num_cook = Cook.objects.count()
+
+    context = {
+        "num_category": num_category,
+        "num_dish": num_dish,
+        "num_cook": num_cook,
+    }
     return render(request, "kitchen/index.html", context=context)
