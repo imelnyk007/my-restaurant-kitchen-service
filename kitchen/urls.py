bdb99359de5b1a03365c4f75643e3f5d7c0ad646
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-20 20:33:32 +0300

    Create html file for update/delete cook

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -12,6 +12,9 @@ from kitchen.views import (
     CookDetailView,
     DishUpdateView,
     DishDeleteView,
+    CookCreateView,
+    CookUpdateView,
+    CookDeleteView,
 )
 
 urlpatterns = [
@@ -26,6 +29,9 @@ urlpatterns = [
     path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
     path("cooks/", CookListView.as_view(), name="cook-list"),
     path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
+    path("cooks/create/", CookCreateView.as_view(), name="cook-create"),
+    path("cooks/<int:pk>/update/", CookUpdateView.as_view(), name="cook-update"),
+    path("cooks/<int:pk>/delete/", CookDeleteView.as_view(), name="cook-delete"),
 
 ]
 
