d56b123ff09f97c9cf7ccf6878ae1ae153131b80
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-20 13:41:06 +0300

    Create models Category, Dish, Cook

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=63)
+
+
+class Cook(AbstractUser):
+    year_of_experience = models.IntegerField()
+    photo = models.ImageField(null=True, black=True, upload_to="cook_photo/")
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=63)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+    picture = models.ImageField(null=True, black=True, upload_to="dish_pictures/")
