d0b352ec3a568e0bc528c5f7a90bd94cbe0c18e1
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-20 14:49:58 +0300

    Add urls to list and detail views

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,5 +1,18 @@
 from django.urls import path
 
+from kitchen.views import (
+    index,
+    CategoryListView,
+    DishListView,
+    DishDetailView, CookListView,
+)
+
 urlpatterns = [
     path("", index, name="index"),
+    path("categories/", CategoryListView.as_view(), name="category-list"),
+    path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dishes/<int:pk>", DishDetailView.as_view(), name="dish-detail"),
+    path("cooks/", CookListView.as_view(), name="cook-list"),
+    path("cooks/<int:pk>", DishDetailView.as_view(), name="cook-detail"),
+
 ]
