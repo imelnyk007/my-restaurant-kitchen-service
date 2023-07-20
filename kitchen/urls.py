9a8b4cd3e91c5ea77e30da0f958a096d3e9d98b3
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-07-20 18:18:11 +0300

    Fix path to cook detail page

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -4,7 +4,9 @@ from kitchen.views import (
     index,
     CategoryListView,
     DishListView,
-    DishDetailView, CookListView,
+    DishDetailView,
+    CookListView,
+    CookDetailView,
 )
 
 urlpatterns = [
@@ -13,7 +15,7 @@ urlpatterns = [
     path("dishes/", DishListView.as_view(), name="dish-list"),
     path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
     path("cooks/", CookListView.as_view(), name="cook-list"),
-    path("cooks/<int:pk>/", DishDetailView.as_view(), name="cook-detail"),
+    path("cooks/<int:pk>/", CookDetailView.as_view(), name="cook-detail"),
 
 ]
 
